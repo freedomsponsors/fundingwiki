42c1f8145aa804638bca165d53dba4a05f92af50
alfinal <al@blogmail.cc>
2025-05-24 10:22:17 +0000

    Remove hardcoded SECRET_KEY

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,9 @@
 from decouple import config
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 DATABASES = {
