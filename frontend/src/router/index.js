8a5c14107d6fe88b19cb6a17304b107c81ff11d0
Tony Lampada <tonylampada@gmail.com>
2026-05-02 22:19:28 -0300

    fix: use vite base for frontend routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,15 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
 import Idea from '@/view/Idea.vue'
 
-const link_prefix = import.meta.env.VITE_LINK_PREFIX
-
 const routes = [
-  { path: ''+link_prefix, name: 'Home', component: Home },
-  { path: link_prefix+'/idea/:id', name: 'Idea', component: Idea },
+  { path: '/', name: 'Home', component: Home },
+  { path: '/idea/:id', name: 'Idea', component: Idea },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
