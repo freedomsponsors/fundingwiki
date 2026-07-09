e10282fb83f801fc480844b5b3c31356c3ce1409
Qingwei Ge <gmoonbiz@qq.com>
2026-07-09 15:20:50 +0800

    Change request User-Agent when call wikidata api.

--- apps/issues/services/wikidata_services.py
+++ apps/issues/services/wikidata_services.py
@@ -11,7 +11,8 @@ def searchConcept(search, language='en', max_count=3):
     
     base_url = "https://www.wikidata.org/w/api.php"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        "User-Agent": "MyVueFundingWikiApp/1.0 (https://vue.funding.wiki; gmoonbiz@funding.wiki)",
         "Accept": "application/json"
     }
     params = {
